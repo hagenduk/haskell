21f48e7c3a2e39832393936d7a554901ab9d78ce
hagenduk <justus-m@hotmail.de>
2015-10-04 22:01:07 +0200

    interval

--- Interval/Data/IntervalSet.hs
+++ Interval/Data/IntervalSet.hs
@@ -11,22 +11,23 @@ type Interval = (Int, Int)
 
 overlap :: Interval -> Interval -> Bool
 overlap (x1, y1) (x2, y2)
-  = undefined
+	| x1 <= x2 = y1+1 >= x2
+	| otherwise = overlap (x2,y2) (x1,y1)
 
 
 less :: Interval -> Interval -> Bool
 less (_x1, y1) (x2, _y2)
-  = undefined
+  = (y1 +1) < x2
 
                            
 emptyInterval :: Interval -> Bool
 emptyInterval (x, y)
-  = undefined
+  = x>y
 
 
 -- merge 2 (overlapping) intervals
 merge :: Interval -> Interval -> Interval
-merge = undefined
+merge (a,b) (c,d)= ((min a c), (max b d))
 
 
 -- ----------------------------------------
@@ -38,7 +39,9 @@ merge = undefined
 type IntervalSet = [Interval]
 
 inv :: IntervalSet -> Bool
-inv = undefined
+inv [] = True
+inv (x:[]) = True
+inv (x1:x2:xs) = (not (emptyInterval x1)) && (not (overlap x1 x2)) && (less x1 x2) && (inv (x2:xs))
 
 
 -- ----------------------------------------
@@ -50,11 +53,18 @@ singleInterval x y
     | otherwise = []
 
 insertInterval :: Interval -> IntervalSet -> IntervalSet
-insertInterval = undefined
+insertInterval (a,b) [] = singleInterval a b
+insertInterval a (x:xs)
+	| overlap a x = insertInterval (merge a x) xs 
+	| less a x = [a, x] ++ xs
+	| otherwise = (x : (insertInterval a xs))
 
 
 fromIntervalList :: [(Int, Int)] -> IntervalSet
-fromIntervalList = undefined
+fromIntervalList xs = fromIntervalList' xs []
+	where
+	fromIntervalList' [] a = a
+	fromIntervalList' (x:xs) a = fromIntervalList' xs (insertInterval x a)
 
 
 -- ----------------------------------------
@@ -71,19 +81,26 @@ insert :: Int -> IntervalSet -> IntervalSet
 insert i = insertInterval (i, i)
 
 union :: IntervalSet -> IntervalSet -> IntervalSet
-union = undefined
+union b [] = b
+union [] a = a
+union (x:xs) a = union xs (insertInterval x a) 
 
 
 member :: Int -> IntervalSet -> Bool
-member = undefined
+member a [] = False
+member a (x:xs) = (overlap' (a,a) x) || (member a xs)
+	where
+		overlap' (x1,y1) (x2,y2) = not (x1 < x2) || (x1 > y2)
 
          
 fromList :: [Int] -> IntervalSet
-fromList = undefined
+fromList [] = []
+fromList (x:xs) = insertInterval (x,x) (fromList xs) 
 
 
 toList :: IntervalSet -> [Int]
-toList = undefined
+toList [] = []
+toList ((a,b):xs) = [a..b] ++ toList xs 
 
 
 -- ----------------------------------------
