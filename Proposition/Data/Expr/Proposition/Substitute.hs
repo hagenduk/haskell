38ea2caa274bce40a7d69875492b58f82d553ce4
hagenduk <justus-m@hotmail.de>
2015-10-07 23:28:41 +0200

    from local

--- Proposition/Data/Expr/Proposition/Substitute.hs
+++ Proposition/Data/Expr/Proposition/Substitute.hs
@@ -10,7 +10,11 @@ import Data.Expr.Proposition.Types
 type VarEnv = [(Ident, Expr)]
 
 substVars :: VarEnv -> Expr -> Expr
-substVars env e = undefined
+--substVars env e = undefined
+substVars env (Lit a) = Lit a
+substVars env (Var a) = snd $ (\[x] -> x) (filter (\x -> (fst x) == a) env)
+substVars env (Unary op1 a) = Unary op1 (substVars env a)
+substVars env (Binary op2 a b) = Binary op2 (substVars env a) (substVars env b)
 
 freeVars :: Expr -> [Ident]
 freeVars  = undefined
