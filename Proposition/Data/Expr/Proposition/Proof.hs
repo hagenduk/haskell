4500c66e6b69346474d12e091667a5c8b9fa18c8
hagenduk <justus-m@hotmail.de>
2015-10-07 23:26:30 +0200

    from local

--- Proposition/Data/Expr/Proposition/Proof.hs
+++ Proposition/Data/Expr/Proposition/Proof.hs
@@ -12,7 +12,10 @@ import           Data.Maybe                       (listToMaybe)
 -- ----------------------------------------
 
 truthTable :: Int -> [[Bool]]
-truthTable n = undefined
+truthTable n
+	| n == 0 = []
+	| n == 1 = [[True], [False]]
+	| otherwise = (map (True:) (truthTable (n-1))) ++ (map (False:) (truthTable (n-1)))
 
 -- compute a proof by generating a truth table,
 -- iterate over all rows in the table
@@ -24,7 +27,15 @@ truthTable n = undefined
 
 proof' :: Expr -> Maybe VarEnv
 proof' e
-  = undefined
+  = proof'' e tt
+	where
+		tt = truthTable (length (freeVars e))
+		proof'' e [] = Nothing
+		proof'' e (x:xs)
+					| not(eval (substVars env e)) = Just env
+					| otherwise = proof'' e xs
+					where
+						env = zip (freeVars e) (map (Lit) x)
 
 
 proof :: Expr -> String
